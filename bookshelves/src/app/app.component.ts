ee2ca3416bede02a042283c1d708db95ba641b23
jordan62300 <jordanfievetpro@outlook.fr>
2019-07-06 22:15:56 +0200

    liasion entre l'application firebase et angular

--- bookshelves/src/app/app.component.ts
+++ bookshelves/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyAW5YE1CahJUDUiZnbfbl4-GeYn-8LeIeQ",
+      authDomain: "angularopenclassroom-57faf.firebaseapp.com",
+      databaseURL: "https://angularopenclassroom-57faf.firebaseio.com",
+      projectId: "angularopenclassroom-57faf",
+      storageBucket: "",
+      messagingSenderId: "881099044593",
+      appId: "1:881099044593:web:7b9d7abbf5c0240c"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
   title = 'bookshelves';
 }
