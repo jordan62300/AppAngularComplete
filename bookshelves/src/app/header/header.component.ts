342bb68319b4feabf8d52b5b3f744dc54ed5c186
jordan62300 <jordanfievetpro@outlook.fr>
2019-07-06 23:49:10 +0200

    ajout de l'authGuardService

--- bookshelves/src/app/header/header.component.ts
+++ bookshelves/src/app/header/header.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import * as firebase from 'firebase'
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  isAuth: boolean;
+
+  constructor( private authService: AuthService) { }
 
   ngOnInit() {
+    console.log(this.isAuth)
+    firebase.auth().onAuthStateChanged(
+      (user) => {
+        if(user)  {
+          this.isAuth = true
+        } else {
+          this.isAuth = false
+        }
+      }
+    )
   }
+
+  onSignOut() {
+    this.authService.signOutUser();
+  }
+
 
 }
